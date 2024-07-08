6267830f0127b253010c177d6ca4500c7c972d8a
nagiyu <otonagiyu@gmail.com>
2024-07-08 21:08:10 +0900

    Add DB Context, Repository, Tests

---
+++ Service/Repositories/SecretRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Repositories
+{
+    public class SecretRepository : ISecretRepository
+    {
+        private readonly SecretDbContext _context;
+
+        public SecretRepository(SecretDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Secret>> GetAllAsync()
+        {
+            return await _context.Secrets.ToListAsync();
+        }
+
+        public async Task<Secret> GetByIdAsync(int id)
+        {
+            return await _context.Secrets.FindAsync(id);
+        }
+
+        public async Task AddAsync(Secret secret)
+        {
+            _context.Secrets.Add(secret);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Secret secret)
+        {
+            _context.Entry(secret).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var secret = await _context.Secrets.FindAsync(id);
+            if (secret != null)
+            {
+                _context.Secrets.Remove(secret);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Service/SecretDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class SecretDbContext : DbContext
+    {
+        public SecretDbContext(DbContextOptions<SecretDbContext> options) : base(options) { }
+
+        public DbSet<Secret> Secrets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Secret>().ToTable("Secrets");
+        }
+    }
+}
